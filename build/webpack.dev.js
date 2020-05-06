ad104caf789630151c259e680f71df0fe49b308d
nieshiming <shiming.nie@aihuishou.com>
2020-05-06 10:51:32 +0800

    style: adjust

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -3,11 +3,11 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 const apiMocker = require('mocker-api');
-const NotifierPlugin = require('friendly-errors-webpack-plugin');
 const notifier = require('node-notifier');
 const chalk = require('chalk');
 
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const NotifierPlugin = require('friendly-errors-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
