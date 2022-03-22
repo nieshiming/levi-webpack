c3945a7bb712c5b94ad8bd3a58d899a76e7db88b
nieshiming <shiming.nie@aihuishou.com>
2022-03-22 20:16:40 +0800

    feat: 数组取交集,双指针解法

--- src/pages/algorithm/basic/array/11.tsx
+++ src/pages/algorithm/basic/array/11.tsx
@@ -35,6 +35,7 @@ export default () => {
 
   // 两个数组的交集 II
   const fn2 = (nums1: number[], nums2: number[]) => {
+    // 利用map
     const result = []
     const map = new Map()
     for (let i = 0; i < nums1.length; i++) {
@@ -49,6 +50,31 @@ export default () => {
     }
 
     return result
+
+    // 利用双指针
+    // 排序sort()的方式是按照ascii码进行排序，它会先将数组里的元素全部转为字符串(不影响原值)，方便比较
+    // const arr = []
+    // nums1.sort((a,b) => a -b)
+    // nums2.sort((a,b) => a -b)
+
+    // let i = 0
+    // let j = 0
+
+    // while (i < nums1.length && j < nums2.length) {
+    //   if (nums1[i] === nums2[j]) {
+    //     arr.push(nums1[i])
+    //     i++
+    //     j++
+    //   }
+    //   if (nums1[i] > nums2[j]) {
+    //     j++
+    //   }
+    //   if (nums1[i] < nums2[j]) {
+    //     i++
+    //   }
+    // }
+
+    // return arr
   }
 
   console.log(fn([1, 2, 2, 1], [2, 2]))
